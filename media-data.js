9746b988ba5d1bd070ead88c89a7739c5e689df5
milanesajade-art <milanesajade@gmail.com>
2026-07-29 20:29:36 -0500

    Add curated product gallery media for Vida pieces

---
+++ media-data.js
@@ -0,0 +1,10 @@
+window.VIDA_PRODUCT_GALLERIES = {
+  'VIDA 004': [
+    'https://drive.google.com/thumbnail?id=1-5tVLig0R4tZ_8RcpMcTnMIWPb7avkWj&sz=w1600',
+    'https://drive.google.com/thumbnail?id=1jRRwLeBcJCNWOBbybR-oz0tPB8JSX6Wo&sz=w1600'
+  ],
+  'VIDA 005': [
+    'https://drive.google.com/thumbnail?id=1FA4GqgSnIixn2i2gXGOV8CrwGIO4FzkK&sz=w1600',
+    'https://drive.google.com/thumbnail?id=1RSch8nHdrAr7a1K8onAa5EcMmLbcGVQD&sz=w1600'
+  ]
+};
