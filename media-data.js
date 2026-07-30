3567c7a9a9103cf6de5fa45a52e414e5e6dfaaa6
milanesajade-art <milanesajade@gmail.com>
2026-07-29 20:59:11 -0500

    Add Flor de Vida Visual V1 gallery

--- media-data.js
+++ media-data.js
@@ -1,4 +1,9 @@
 window.VIDA_PRODUCT_GALLERIES = {
+  'VIDA 001': [
+    'https://drive.google.com/thumbnail?id=1STYhz_iuJAu4Yjxh0-z_c2SWWrG1y-_t&sz=w1600',
+    'https://drive.google.com/thumbnail?id=1TXvHxNeUR0HJX3nKU-OvX5o334PBG8Yw&sz=w1600',
+    'https://drive.google.com/thumbnail?id=1huT8CbOiMvY9cteDC3LxFvkjUivuDanG&sz=w1600'
+  ],
   'VIDA 004': [
     'https://drive.google.com/thumbnail?id=1-5tVLig0R4tZ_8RcpMcTnMIWPb7avkWj&sz=w1600',
     'https://drive.google.com/thumbnail?id=1jRRwLeBcJCNWOBbybR-oz0tPB8JSX6Wo&sz=w1600',
