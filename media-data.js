ba105295c6a8c34195fffddf481438109afafdc0
milanesajade-art <milanesajade@gmail.com>
2026-07-29 20:46:55 -0500

    Expand Visual V1 product galleries

--- media-data.js
+++ media-data.js
@@ -1,7 +1,8 @@
 window.VIDA_PRODUCT_GALLERIES = {
   'VIDA 004': [
     'https://drive.google.com/thumbnail?id=1-5tVLig0R4tZ_8RcpMcTnMIWPb7avkWj&sz=w1600',
-    'https://drive.google.com/thumbnail?id=1jRRwLeBcJCNWOBbybR-oz0tPB8JSX6Wo&sz=w1600'
+    'https://drive.google.com/thumbnail?id=1jRRwLeBcJCNWOBbybR-oz0tPB8JSX6Wo&sz=w1600',
+    'https://drive.google.com/thumbnail?id=1ym5L548RdXNaUoGPskpuIWh31zRJKsqn&sz=w1600'
   ],
   'VIDA 005': [
     'https://drive.google.com/thumbnail?id=1FA4GqgSnIixn2i2gXGOV8CrwGIO4FzkK&sz=w1600',
